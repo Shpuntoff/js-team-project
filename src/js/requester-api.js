f6a94013ff2efd8ea8c8c85cf24af8862cfc3639
ArthurKorchak <korchak.arthur@gmail.com>
2022-08-22 23:29:40 +0300

    upd

--- src/js/requester-api.js
+++ src/js/requester-api.js
@@ -1,10 +1,5 @@
-export function requesterAPI(requst = 0, numOfPage = 1) {
-    if (requst) {
-        ref = ``
-    }
-
-
-    fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=1e7c9642f723687d0411097a32c8e17f&page=${numOfPage}`)
+export function requesterApi(requst = '', numOfPage = 1) {
+    fetch(`https://api.themoviedb.org/3/trending/all/day?api_key=1e7c9642f723687d0411097a32c8e17f&page=${numOfPage}&query=${requst}&language=en-US`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(response.status);
@@ -12,8 +7,21 @@ export function requesterAPI(requst = 0, numOfPage = 1) {
             return response.json();
         })
         .then(response => {
+            return response;
+        })
+        .catch(error => console.log(error));
+};
 
-            console.log(response.results[0])
+export function requesterApiByID(id) {
+    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=1e7c9642f723687d0411097a32c8e17f&language=en-US`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        })
+        .then(response => {
+            return response;
         })
         .catch(error => console.log(error));
 };
--- src/my-library.js
+++ src/my-library.js
@@ -1,1 +1,2 @@
 import { watchedMovies, queueMovies } from './js/user-storage.js';
+import { requesterApi, requesterApiByID } from './js/requester-api.js';
