0fdfb430c7fced10a0f2e348608636cfef0540ac
ArthurKorchak <korchak.arthur@gmail.com>
2022-08-22 20:06:29 +0300

    upd

---
+++ src/js/user-storage.js
@@ -0,0 +1,10 @@
+
+const targetForAdd = document.querySelector('body');
+
+targetForAdd.innerHTML = 
+`<div>
+    <img>
+    <h2></h2>
+    <p>Vote / Votes<span>${vote}</span><span>${votes}</span></p>
+    <p>Popularyty<span>${pop}</span></p>
+</div>`
