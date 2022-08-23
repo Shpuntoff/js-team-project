16585d55538692415e1fd079822b3869580184a6
Myrhoiazov <denis.mirgoyazov@gmail.com>
2022-08-23 19:21:41 +0300

    add: theme dark-light

--- src/my-library.js
+++ src/my-library.js
@@ -1,6 +1,8 @@
 import { watchedMovies, queueMovies } from './js/user-storage.js';
 import { requesterApi, requesterApiByID, requesterApiGenres } from './js/requester-api.js';
 import { renderHomeCards, renderLibraryCards, renderModal } from './js/render.js';
+import { btn } from './js/theme-lite-dark.js'
+
 
 requesterApiGenres()
 requesterApi()
