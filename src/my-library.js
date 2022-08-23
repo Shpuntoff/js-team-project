6413a88a2ce13f7ec9dc79965a101d04507c1d3a
ArthurKorchak <korchak.arthur@gmail.com>
2022-08-23 17:39:03 +0300

    upd

--- src/my-library.js
+++ src/my-library.js
@@ -2,11 +2,31 @@ import { watchedMovies, queueMovies } from './js/user-storage.js';
 import { requesterApi, requesterApiByID, requesterApiGenres } from './js/requester-api.js';
 import { renderHomeCards, renderLibraryCards, renderModal, renderWatchedOrQueue } from './js/render.js';
 
-requesterApiGenres()
-requesterApi()
-    .then(data => {
-        renderLibraryCards(data.results)
+const btns = document.querySelectorAll('.btn')
+btns.forEach((elem) => {
+    elem.addEventListener('click', (event) => {
+        btns.forEach((elem) => {
+            elem.classList.toggle('btn--active')
+        })
+
+
+        console.log(event.target.textContent);
     })
+})
+
+JSON.parse(localStorage.getItem(`watchedMoviesIDs`))
+    .forEach(elem => {
+        requesterApiByID(elem)
+            .then(data => {
+                renderWatchedOrQueue(data)
+            })
+    });
+
+// requesterApiGenres()
+// requesterApi()
+//     .then(data => {
+//         renderLibraryCards(data.results)
+//     })
 
 
 // requesterApiByID(361743)
@@ -16,13 +36,7 @@ requesterApi()
 
 // watchedMovies('put', 755566)
 
-// JSON.parse(localStorage.getItem(`watchedMoviesIDs`))
-//     .forEach(elem => {
-//         requesterApiByID(elem)
-//             .then(data => {
-//                 renderWatchedOrQueue(data)
-//             })
-//     });
+
 
 // JSON.parse(localStorage.getItem(`queueMoviesIDs`))
 //     .forEach(elem => {
