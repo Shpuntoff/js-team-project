99340e4a41699b05c285ec3da43a26b0202e7e6b
ArthurKorchak <korchak.arthur@gmail.com>
2022-08-23 09:51:15 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import NewApiService from './js/apiSevice';
+import { watchedMovies, queueMovies } from './js/user-storage.js';
+import { requesterApi, requesterApiByID, requesterApiGenres } from './js/requester-api.js';
+import { renderHomeCards, renderLibraryCards, renderModal } from './js/render.js';
+
+requesterApi()
+    .then(data => {
+        console.log(data.results);
+        renderHomeCards(data.results)
+    })
