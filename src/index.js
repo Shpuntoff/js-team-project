735ab97a8918b76c644f880d7390e6a6f08a078c
ArthurKorchak <korchak.arthur@gmail.com>
2022-08-25 11:59:39 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import { requesterApi, requesterApiGenres } from './js/requester-api.js';
-import { renderHomeCards} from './js/render.js';
+
+import { watchedMovies, queueMovies } from './js/user-storage.js';
+import { requesterApi, requesterApiByID, requesterApiGenres } from './js/requester-api.js';
+import { renderHomeCards, renderLibraryCards, renderModal } from './js/render.js';
 import {renderPagination} from './js/pagination.js';
+import { btn } from './js/theme-lite-dark.js'
+import {scrollBtn} from './js/scroll.js'
+import {spinner} from'./js/spinner.js'
 
 requesterApiGenres();
 requesterApi()
