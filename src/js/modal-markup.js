2ae82ea9e2dd23e20139a6bfb883f8025a8d6aba
Shpuntoff <v.shpuntoff@gmail.com>
2022-08-25 18:11:01 +0300

    upd

--- src/js/modal-markup.js
+++ src/js/modal-markup.js
@@ -22,35 +22,43 @@ export function modalLibraryMarkup({
     } else {
         posterUrl = `https://image.tmdb.org/t/p/original/${poster_path}`;
     }
+    let aboutText = "Sorry! We don't have a description for this movie yet.";
+    if(overview.length > 0){
+      aboutText = overview;
+    }
 
     return `<div class="movie-modal__content">
       <img class="movie-modal__img" src="${posterUrl}" alt="${title}"></img>
   <div class="movie-modal__text">
     <h2 class="movie-modal__title">${title}</h2>
-      <div class="movie-modal__figures">
-      <ul class="movie-modal__properties list">
-        <li>Vote / Votes</li>
-        <li>Popularity</li>
-        <li>Original Title</li>
-        <li>Genre</li>
+    <table>
+  <tbody>
+    <tr>
+      <td class="movie-modal__properties space">Vote / Votes</td>
+      <td class="movie-modal__values"><span class="movie-modal__vote-average">${vote_average.toFixed(
+        1
+    )}</span> / <span class="movie-modal__vote-count">${vote_count}</span></td>
+    </tr>
+    <tr>
+      <td class="movie-modal__properties space">Popularity</td>
+      <td class="movie-modal__values">${popularity.toFixed(1)}</td>
+    </tr>
+    <tr>
+      <td class="movie-modal__properties space">Original Title</td>
+      <td class="movie-modal__values">${original_title}</td>
+    </tr>
+    <tr>
+    <td class="movie-modal__properties">Genre</td>
+    <td class="movie-modal__values">${movieGenres}</td>
+  </tr>
+  </tbody>
+</table>
+      <p class="movie-modal__about--title">ABOUT</p>
+      <p class="movie-modal__about--text">${aboutText}</p>
+      <ul class="modal__btns">
+        <li><button type="button" class="modal__btns__button js-addtowatched">add to watched</button></li>
+        <li><button type="button" class="modal__btns__button js-addtoqueue">add to queue</button></li>
       </ul>
-      <ul class="movie-modal__values list">
-        <li><span class="movie-modal__vote-average">${vote_average.toFixed(
-            1
-        )}</span> / <span class="movie-modal__vote-count">${vote_count}</span></li>
-        <li>${popularity.toFixed(1)}</li>
-        <li class="movie-modal__title-original">${original_title}</li>
-        <li>${movieGenres}</li>
-      </ul>
-      </div>
-      
-      <h3 class="movie-modal__about--title">ABOUT</h3>
-      <p class="movie-modal__about--text">${overview}</p>
-      
-      <div class="modal__btns">
-        <button type="button" class="modal__btns__button js-addtowatched">add to watched</button>
-        <button type="button" class="modal__btns__button js-addtoqueue">add to queue</button>
-      </div>
   </div>
 </div>
     `;
